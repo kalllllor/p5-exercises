79e221b1c097648782480b497ba989bdef3d5543
kalllllor <karolgren3@gmail.com>
2021-09-07 15:28:21 +0200

    basic form finished

--- sketch.js
+++ sketch.js
@@ -1,24 +1,20 @@
 let video;
 let vScale = 2;
 let particles = [];
-
+let x = 0;
 function setup() {
-  createCanvas(640, 480);
+  createCanvas(960, 240);
   pixelDensity(1);
   video = createCapture(VIDEO);
-  video.size(width, height);
-
-  for (let i = 0; i < 10; i++) {
-    particles.push(
-      new Particle(random(width), random(height))
-    );
-  }
+  video.size(320, 240);
+  background(50);
 }
 
 function draw() {
   video.loadPixels();
-  for (let i = 0; i < particles.length; i++) {
-    particles[i].update();
-    particles[i].show();
-  }
+  let w = video.width;
+  let h = video.height;
+  copy(video, w / 2, 0, 1, h, x, 0, 1, h);
+
+  x > width ? (x = 0) : (x += 1);
 }
